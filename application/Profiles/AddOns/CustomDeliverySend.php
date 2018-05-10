b4fd7bba9c680ae11da8104d50fa8bdb0d8ea67a
Jamie Wilson <jamiethetec@gmail.com>
2018-05-10 12:32:00 -0700

    Saving the info in database nwo

---
+++ application/Profiles/AddOns/CustomDeliverySend.php
@@ -0,0 +1,143 @@
+<head>
+    <?php
+        include("application/DBConnect/dbconnect.php");
+	    session_start();
+        $UserID = $_SESSION['ID']; 
+        $number = $_SESSION['ItemNumber'];
+        $PickUp = $_SESSION['PickUp'];
+        $PickPerson = $_SESSION['PickPerson'];
+        $DropOff = $_SESSION['DropOff'];
+        $DropPerson = $_SESSION['DropPerson'];
+        $DecItem = $_SESSION['DecItem'];
+
+
+	    if ($PickUp >"0" AND $DropOff >"0") {
+	      $PickUpsend_sql="SELECT * FROM pickup WHERE Name='$PickUp' AND User_ID='$UserID'";
+   	      $PickUpsend_query=mysqli_query($dbconnect, $PickUpsend_sql);
+	      $PickUpsend_rs=mysqli_fetch_assoc($PickUpsend_query);
+
+	      $DropOffsend_sql="SELECT * FROM dropoff WHERE Name='$DropOff' AND User_ID='$UserID'";
+   	      $DropOffsend_query=mysqli_query($dbconnect, $DropOffsend_sql);
+	      $DropOffsend_rs=mysqli_fetch_assoc($DropOffsend_query);
+
+	      $PName = $PickUpsend_rs['Name'];
+	      $PNumber = $PickUpsend_rs['Number'];
+	      $PStreet = $PickUpsend_rs['Street'];
+	      $PApt = $PickUpsend_rs['AptNumber'];
+	      $PCity = $PickUpsend_rs['City'];
+	      $PZip = $PickUpsend_rs['Zip'];
+	      $Pcomment = $PickUpsend_rs['comment'];
+
+		  $DName = $PickUpsend_rs['Name'];
+	      $DNumber = $PickUpsend_rs['Number'];
+	      $DStreet = $PickUpsend_rs['Street'];
+	      $DApt = $PickUpsend_rs['AptNumber'];
+	      $DCity = $PickUpsend_rs['City'];
+	      $DZip = $PickUpsend_rs['Zip'];
+	      $Dcomment = $PickUpsend_rs['comment'];
+
+	      $CSAdress_sql="INSERT INTO customdelivery (User_ID, PickAdressNumber, PickStreet, PickAptNumber, PickZip, PickCity, Pickcomment, DropAdressNumber, DropStreet, DropAptNumber, DropZip, DropCity, Dropcomment) VALUES ('$UserID',  '$PNumber', '$PStreet', '$PApt', '$PZip', '$PCity',  '$Pcomment', '$DNumber', '$DStreet', '$DApt', '$DZip', '$DCity',  '$Dcomment')";
+	      $CSAdress_query=mysqli_query($mysqli, $CSAdress_sql);
+		} elseif ($PickUp <"0" AND $DropOff <"0") {
+  	          $name = $_SESSION["name"];
+              $street_number = $_SESSION["street_number"];  
+              $route= $_SESSION["route"];
+              $town = $_SESSION["locality"];
+              $zip = $_SESSION["postal_code"];
+              $country = $_SESSION["country"];
+	          $Comments = $_SESSION["Comments"];
+              $State = $_SESSION["State"];
+              $AptNumber = $_SESSION["AptNumber"];
+  	          $name5 = $_SESSION["name5"];
+              $street_number5 = $_SESSION["street_number5"];  
+              $route5= $_SESSION["route5"];
+              $town5 = $_SESSION["locality5"];
+              $zip5 = $_SESSION["postal_code5"];
+              $country5 = $_SESSION["country5"];
+	          $Comments5 = $_SESSION["Comments5"];
+              $State5 = $_SESSION["State5"];
+              $AptNumber5 = $_SESSION["AptNumber5"];
+	          $CAddress_sql="INSERT INTO customdelivery (User_ID, PickAdressNumber, PickStreet, PickAptNumber, PickZip, PickCity, Pickcomment, DropAdressNumber, DropStreet, DropAptNumber, DropZip, DropCity, Dropcomment) VALUES ('$UserID', '$street_number', '$route', '$AptNumber', '$zip', '$town',  '$Comments', '$street_number5', '$route5', '$AptNumber5', '$zip5', '$town5',  '$Comments5')";
+	          $CAddress_query=mysqli_query($mysqli, $CAddress_sql);
+	          $useraddress_sql="INSERT INTO pickup (User_ID, Name, Number, Street, AptNumber, Zip, City, comment) VALUES ('$UserID', '$name', '$street_number', '$route', '$AptNumber', '$zip', '$town',  '$Comments')";
+	          $useraddress_query=mysqli_query($mysqli, $useraddress_sql);
+	          $DropOffAdress_sql="INSERT INTO dropoff (User_ID, Name, Number, Street, AptNumber, Zip, City, comment) VALUES ('$UserID', '$name5', '$street_number5', '$route5', '$AptNumber5', '$zip5', '$town5',  '$Comments5')";
+	          $DropOffAdress_query=mysqli_query($mysqli, $DropOffAdress_sql);
+
+	    } elseif ($PickUp <"0" AND $DropOff >"0") {
+
+	      $DropOffsend_sql="SELECT * FROM dropoff WHERE Name='$DropOff' AND User_ID='$UserID'";
+   	      $DropOffsend_query=mysqli_query($dbconnect, $DropOffsend_sql);
+	      $DropOffsend_rs=mysqli_fetch_assoc($DropOffsend_query);
+
+
+  	      $name = $_SESSION["name"];
+          $street_number = $_SESSION["street_number"];  
+          $route= $_SESSION["route"];
+          $town = $_SESSION["locality"];
+          $zip = $_SESSION["postal_code"];
+          $country = $_SESSION["country"];
+	      $Comments = $_SESSION["Comments"];
+          $State = $_SESSION["State"];
+          $AptNumber = $_SESSION["AptNumber"];
+
+
+		  $DName = $DropOffsend_rs['Name'];
+	      $DNumber = $DropOffsend_rs['Number'];
+	      $DStreet = $DropOffsend_rs['Street'];
+	      $DApt = $DropOffsend_rs['AptNumber'];
+	      $DCity = $DropOffsend_rs['City'];
+	      $DZip = $DropOffsend_rs['Zip'];
+	      $Dcomment = $DropOffsend_rs['comment'];
+
+	      $CSAdress_sql="INSERT INTO customdelivery (User_ID, PickAdressNumber, PickStreet, PickAptNumber, PickZip, PickCity, Pickcomment, DropAdressNumber, DropStreet, DropAptNumber, DropZip, DropCity, Dropcomment) VALUES ('$UserID', '$street_number', '$route', '$AptNumber', '$zip', '$town',  '$Comments', '$DNumber', '$DStreet', '$DApt', '$DZip', '$DCity',  '$Dcomment')";
+	      $CSAdress_query=mysqli_query($mysqli, $CSAdress_sql);
+
+	      $useraddress_sql="INSERT INTO pickup (User_ID, Name, Number, Street, AptNumber, Zip, City, comment) VALUES ('$UserID', '$name', '$street_number', '$route', '$AptNumber', '$zip', '$town',  '$Comments')";
+	      $useraddress_query=mysqli_query($mysqli, $useraddress_sql);
+
+		} else {
+
+
+	      $PickUpsend_sql="SELECT * FROM pickup WHERE Name='$PickUp' AND User_ID='$UserID'";
+   	      $PickUpsend_query=mysqli_query($dbconnect, $PickUpsend_sql);
+	      $PickUpsend_rs=mysqli_fetch_assoc($PickUpsend_query);
+
+	      $PName = $PickUpsend_rs['Name'];
+	      $PNumber = $PickUpsend_rs['Number'];
+	      $PStreet = $PickUpsend_rs['Street'];
+	      $PApt = $PickUpsend_rs['AptNumber'];
+	      $PCity = $PickUpsend_rs['City'];
+	      $PZip = $PickUpsend_rs['Zip'];
+	      $Pcomment = $PickUpsend_rs['comment'];
+
+  	      $name5 = $_SESSION["name5"];
+          $street_number5 = $_SESSION["street_number5"];  
+          $route5= $_SESSION["route5"];
+          $town5 = $_SESSION["locality5"];
+          $zip5 = $_SESSION["postal_code5"];
+          $country5 = $_SESSION["country5"];
+	      $Comments5 = $_SESSION["Comments5"];
+          $State5 = $_SESSION["State5"];
+          $AptNumber5 = $_SESSION["AptNumber5"];
+
+
+	      $CAddress_sql="INSERT INTO customdelivery (User_ID, PickAdressNumber, PickStreet, PickAptNumber, PickZip, PickCity, Pickcomment, DropAdressNumber, DropStreet, DropAptNumber, DropZip, DropCity, Dropcomment) VALUES ('$UserID',  '$PNumber', '$PStreet', '$PApt', '$PZip', '$PCity',  '$Pcomment', '$street_number5', '$route5', '$AptNumber5', '$zip5', '$town5',  '$Comments5')";
+	      $CAddress_query=mysqli_query($mysqli, $CAddress_sql);
+	      $DropOffAdress_sql="INSERT INTO dropoff (User_ID, Name, Number, Street, AptNumber, Zip, City, comment) VALUES ('$UserID', '$name5', '$street_number5', '$route5', '$AptNumber5', '$zip5', '$town5',  '$Comments5')";
+	      $DropOffAdress_query=mysqli_query($mysqli, $DropOffAdress_sql);
+		}
+
+   include 'css/css.html'; 
+   
+    ?>
+	<link href="application/css/Profiles.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+  <div class="form">
+  <h1>Thank You</h1>
+  <h2>We sent you an email and we will get a hold of your as so as we can</h2>
+  <a href="index.php?page=application/Profiles/Admin"><button class="button button-block" type="submit" style="margin-top:10px;">Go Back To Profile</button></a>
+  </div>
+</body>
--- application/Profiles/AddOns/CustomDeliverySure.php
+++ application/Profiles/AddOns/CustomDeliverySure.php
@@ -1,64 +1,73 @@
 <head>
     <?php
+	 session_start();
+    $UserID = $_SESSION['ID'];
       include("application/DBConnect/dbconnect.php");
       include 'css/css.html'; 
 	  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	  	$ItemNumber = $_POST["ItemNumber"];
-	  	$PickUp = $_POST["PickUp"];
-		$PickPerson = $_POST["PickPerson"];
-	  	$DropOff = $_POST["DropOff"];
-		$DropPerson = $_POST["DropPerson"];
-		$DecItem = $_POST["DecItem"];
+        $_SESSION['ItemNumber'] = $_POST["ItemNumber"];
+        $_SESSION['PickUp'] = $_POST["PickUp"];
+        $_SESSION['PickPerson'] = $_POST["PickPerson"];
+        $_SESSION['DropOff'] = $_POST["DropOff"];
+        $_SESSION['DropPerson'] = $_POST["DropPerson"];
+        $_SESSION['DecItem'] = $_POST["DecItem"];
+
+		$PickUp = $_POST['PickUp'];
+        $DropOff = $_POST["DropOff"];
 	  } 
-	  $PickUpsend_sql="SELECT * FROM pickup WHERE Name='$PickUp'";
+	  $PickUpsend_sql="SELECT * FROM pickup WHERE Name='$PickUp' AND User_ID='$UserID'";
    	  $PickUpsend_query=mysqli_query($dbconnect, $PickUpsend_sql);
 	  $PickUpsend_rs=mysqli_fetch_assoc($PickUpsend_query);
 
-	  $DropOffsend_sql="SELECT * FROM dropoff WHERE Name='$DropOff'";
+	  $DropOffsend_sql="SELECT * FROM dropoff WHERE Name='$DropOff' AND User_ID='$UserID'";
    	  $DropOffsend_query=mysqli_query($dbconnect, $DropOffsend_sql);
 	  $DropOffsend_rs=mysqli_fetch_assoc($DropOffsend_query);
 
-	  $NUmber = $PickUpsend_rs['Number'];
-	  $DropNumber = $DropOffsend_rs['Number'];
+	  $NUmber = $PickUpsend_rs['ID'];
+      $DropNumber = $DropOffsend_rs['ID'];
+        $_SESSION['PickID'] = $NUmber;
+        $_SESSION['DropID'] = $DropNumber;
+		echo $UserID;
     ?>
 	<link href="application/css/Profiles.css" rel="stylesheet" type="text/css" />
 </head>
 <div class="form">
+<form  action="index.php?page=application/Profiles/AddOns/CustomDeliverySend" method="post">
     <h1>Is this right</h1>
 	<h2>If not just correct it then send it</h2>
     <?php
             if ($NUmber<"0") { 
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  	          $name = $_POST["name"];
-              $Stnumber = $_POST["street_number"];  
-              $street = $_POST["route"];
-              $town = $_POST["locality"];
-              $zip = $_POST["postal_code"];
-              $country = $_POST["country"];
-	          $Comments = $_POST["Comments"];
-              $State = $_POST["State"];
-              $AptNumber = $_POST["AptNumber"];
+  	          $_SESSION['name'] = $_POST["name"];
+              $_SESSION['street_number'] = $_POST["street_number"];  
+              $_SESSION['route'] = $_POST["route"];
+              $_SESSION['locality'] = $_POST["locality"];
+              $_SESSION['postal_code'] = $_POST["postal_code"];
+              $_SESSION['country'] = $_POST["country"];
+	          $_SESSION['Comments'] = $_POST["Comments"];
+              $_SESSION['State'] = $_POST["State"];
+              $_SESSION['AptNumber'] = $_POST["AptNumber"];
 		    }
     ?>
     <table id="addaddress">
     	<tr>
-		   <div style="margin-top:10px;"><greentext>Pickin Up at: <?php echo $name;?></greentext></div>
-           <td class="slimField"><input placeholder="<?php echo $Stnumber;?>" class="field" disabled="true"></input></td>
-           <td class="wideField" colspan="2"><input placeholder="<?php echo $street;?>" class="field" disabled="true"></input></td>
+		   <div style="margin-top:10px;"><greentext>Pickin Up at: <?php echo $_SESSION['name'];?></greentext></div>
+           <td class="slimField"><input placeholder="<?php echo $_SESSION['street_number'];?>" class="field" disabled="true"></input></td>
+           <td class="wideField" colspan="2"><input placeholder="<?php echo $_SESSION['route'];?>" class="field" disabled="true"></input></td>
         </tr>
         <tr>
-	      <td class="slimField"><input class="field" placeholder="<?php echo $AptNumber;?>" disabled="true"></input></td>
-          <td class="wideField" colspan="2"><input class="field" placeholder="<?php echo $town;?>" disabled="true"></input></td>
+	      <td class="slimField"><input class="field" name="AptNumber" placeholder="<?php echo $_SESSION['AptNumber'];?>" disabled="true"></input></td>
+          <td class="wideField" colspan="2"><input class="field" name="locality" placeholder="<?php echo $_SESSION['locality'];?>" disabled="true"></input></td>
         </tr>
         <tr>
-          <td class="slimField"><input class="field" placeholder="<?php echo $State;?>"  disabled="true"></input></td>
-          <td class="wideField"><input class="field" placeholder="<?php echo $zip;?>"  disabled="true"></input></td>
+          <td class="slimField"><input class="field" name="State" placeholder="<?php echo $_SESSION['State'];?>"  disabled="true"></input></td>
+          <td class="wideField"><input class="field" name="postal_code" placeholder="<?php echo $_SESSION['postal_code'];?>"  disabled="true"></input></td>
         </tr>
         <tr>
-          <td class="wideField" colspan="3"><input class="field" id="country" placeholder="<?php echo $country;?>" disabled="true"></input></td>
+          <td class="wideField" colspan="3"><input class="field" name="country" placeholder="<?php echo $_SESSION['country'];?>" disabled="true"></input></td>
         </tr>
         <tr>
-          <td class="wideField" colspan="3"><textarea class="field" placeholder="<?php echo $Comments;?>" ></textarea></td>
+          <td class="wideField" colspan="3"><textarea class="field" name="Comments" placeholder="<?php echo $_SESSION['Comments'];?>" ></textarea></td>
         </tr>
 		<tr>
            <td class="wideField" colspan="2"><input placeholder="<?php echo $PickPerson;?>" class="field" disabled="true"></input></td>
@@ -69,12 +78,12 @@
     ?>
     <table id="addaddress">
     	<tr>
-		   <div style="margin-top:10px;"><greentext>Pickin Up at: <?php echo $PickUp;?></greentext></div>
-           <td class="slimField"><input placeholder="<?php echo $PickUpsend_rs['Number'];?>" class="field" disabled="true"></input></td>
-           <td class="wideField" colspan="2"><input placeholder="<?php echo $PickUpsend_rs['Street'];?>" class="field" disabled="true"></input></td>
+		   <div style="margin-top:10px;"><greentext>Pickin Up at:<?php echo $_SESSION['PickUp'];?></greentext></div>
+           <td class="slimField"><input name="SavedPickNumber" placeholder="<?php echo $PickUpsend_rs['Number'];?>" class="field"  disabled="true"></input></td>
+           <td class="wideField" colspan="2"><input name="SavedPickStreet" placeholder="<?php echo $PickUpsend_rs['Street'];?>" class="field" disabled="true"></input></td>
         </tr>
 		<tr>
-           <td class="wideField" colspan="2"><input placeholder="<?php echo $PickPerson;?>" class="field" disabled="true"></input></td>
+           <td class="wideField" colspan="2"><input placeholder="<?php echo $_SESSION['PickPerson'];?>" class="field" disabled="true"></input></td>
 		</tr>
     </table>
     <?php 
@@ -83,6 +92,20 @@
     <?php
             if ($DropNumber<"0") { 
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  	          $_SESSION['name5'] = $_POST["name5"];
+              $_SESSION['street_number5'] = $_POST["street_number5"];  
+              $_SESSION['route5'] = $_POST["route5"];
+              $_SESSION['locality5'] = $_POST["locality5"];
+              $_SESSION['postal_code5'] = $_POST["postal_code5"];
+              $_SESSION['country5'] = $_POST["country5"];
+	          $_SESSION['Comments5'] = $_POST["Comments5"];
+              $_SESSION['State5'] = $_POST["State5"];
+              $_SESSION['AptNumber5'] = $_POST["AptNumber5"];
+
+
+
+
+
   	          $name5 = $_POST["name5"];
               $Stnumber5 = $_POST["street_number5"];  
               $street5 = $_POST["route5"];
@@ -96,26 +119,26 @@
 	?>
     <table id="addaddress">
     	<tr>
-		   <div style="margin-top:10px;"><greentext>Pickin Up at: <?php echo $name5;?></greentext></div>
-           <td class="slimField"><input placeholder="<?php echo $Stnumber5;?>" class="field" disabled="true"></input></td>
-           <td class="wideField" colspan="2"><input placeholder="<?php echo $street5;?>" class="field" disabled="true"></input></td>
+		   <div style="margin-top:10px;"><greentext>Droping Off at: <?php echo $_SESSION['name5'];?></greentext></div>
+           <td class="slimField"><input placeholder="<?php echo $_SESSION['street_number5'];?>" class="field" disabled="true"></input></td>
+           <td class="wideField" colspan="2"><input placeholder="<?php echo $_SESSION['route5'];?>" class="field" disabled="true"></input></td>
         </tr>
         <tr>
-	      <td class="slimField"><input class="field" placeholder="<?php echo $AptNumber5;?>" disabled="true"></input></td>
-          <td class="wideField" colspan="2"><input class="field" placeholder="<?php echo $town5;?>" disabled="true"></input></td>
+	      <td class="slimField"><input class="field" placeholder="<?php echo $_SESSION['AptNumber5'];?>" disabled="true"></input></td>
+          <td class="wideField" colspan="2"><input class="field" placeholder="<?php echo $_SESSION['locality5'];?>" disabled="true"></input></td>
         </tr>
         <tr>
-          <td class="slimField"><input class="field" placeholder="<?php echo $State5;?>"  disabled="true"></input></td>
-          <td class="wideField"><input class="field" placeholder="<?php echo $zip5;?>"  disabled="true"></input></td>
+          <td class="slimField"><input class="field" placeholder="<?php echo $_SESSION['State5'];?>"  disabled="true"></input></td>
+          <td class="wideField"><input class="field" placeholder="<?php echo $_SESSION['postal_code5'];?>"  disabled="true"></input></td>
         </tr>
         <tr>
-          <td class="wideField" colspan="3"><input class="field" id="country" placeholder="<?php echo $country5;?>" disabled="true"></input></td>
+          <td class="wideField" colspan="3"><input class="field" id="country" placeholder="<?php echo $_SESSION['country5'];?>" disabled="true"></input></td>
         </tr>
         <tr>
-          <td class="wideField" colspan="3"><textarea class="field" placeholder="<?php echo $Comments5;?>" ></textarea></td>
+          <td class="wideField" colspan="3"><textarea class="field" placeholder="<?php echo $_SESSION['Comments5'];?>" ></textarea></td>
         </tr>
 		<tr>
-           <td class="wideField" colspan="2"><input placeholder="<?php echo $DropPerson;?>" class="field" disabled="true"></input></td>
+           <td class="wideField" colspan="2"><input placeholder="<?php echo $_SESSION['DropPerson'];?>" class="field" disabled="true"></input></td>
 		</tr>
     </table>
     <?php 
@@ -123,12 +146,12 @@
 	?>
     <table id="addaddress">
     	<tr>
-           <div style="margin-top:10px;"><greentext>Droppin off at: <?php echo $DropOff;?></greentext></div>
+           <div style="margin-top:10px;"><greentext>Droppin off at: <?php echo $_SESSION['DropOff'];?></greentext></div>
            <td class="slimField"><input placeholder="<?php echo $DropOffsend_rs['Number'];?>" class="field" disabled="true"></input></td>
            <td class="wideField" colspan="2"><input placeholder="<?php echo $DropOffsend_rs['Street'];?>" class="field" disabled="true"></input></td>
         </tr>
 		<tr>
-           <td class="wideField" colspan="2"><input placeholder="<?php echo $DropPerson;?>" class="field" disabled="true"></input></td>
+           <td class="wideField" colspan="2"><input placeholder="<?php echo $_SESSION['DropPerson'];?>" class="field" disabled="true"></input></td>
 		</tr>
 		<?php 
 		   } 
@@ -137,12 +160,13 @@
     <table id="addaddress">
     	<tr>
 		   <td class="slimField"><greentext>Item number</greentext></td>
-           <td class="wideField" colspan="1"><input placeholder="<?php echo $ItemNumber;?>" class="field" disabled="true"></input></td>
+           <td class="wideField" colspan="1"><input placeholder="<?php echo $_SESSION['ItemNumber'];?>" class="field" disabled="true"></input></td>
         </tr>
     	<tr>
-           <td class="wideField" colspan="3"><textarea placeholder="<?php echo $DecItem;?>" class="field" disabled="true"></textarea></td>
+           <td class="wideField" colspan="3"><textarea placeholder="<?php echo $_SESSION['DecItem'];?>" class="field" disabled="true"></textarea></td>
         </tr>
     </table>
     <button class="button button-block" type="submit" style="margin-top:10px;">Send Your deliver</button>
+</form>
     <button onclick="history.go(-1);" style="margin-top:10px;" class="button button-block"/>Back</button>
 </div>
